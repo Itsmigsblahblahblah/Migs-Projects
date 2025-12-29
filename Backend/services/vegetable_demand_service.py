bf3ef19fb224eefb6b6d30a6964a1c1d52552d79
mabs08 <mabs.dm12@gmail.com>
2025-12-29 13:11:36 +0800

    fix runtime error

--- Backend/services/vegetable_demand_service.py
+++ Backend/services/vegetable_demand_service.py
@@ -8,6 +8,7 @@ import logging
 import time
 import numpy as np
 import pandas as pd
+from collections import OrderedDict
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error
